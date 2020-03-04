e582e697dc22fd38aaf815debbd1547b4ff6536a
Romain Buquet <1444382+rbuquet@users.noreply.github.com>
2020-03-05 00:34:13 +0100

    Adding my sources

---
+++ src/main/java/fr/rbt/vcard2clibase/App.java
@@ -0,0 +1,58 @@
+package fr.rbt.vcard2clibase;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.URLEncoder;
+import java.util.List;
+
+import ezvcard.VCard;
+import ezvcard.io.text.VCardReader;
+import ezvcard.property.Telephone;
+
+/**
+ * À utiliser en mode flux UNIX, comme par exemple :
+ * cat ~/Downloads/contacts.vcf | java -DURL_START="http://blahost/toto?aa=bb" -DKEY_NAME=NOM -DKEY_TEL=TELEPHON -jar target/vcard2clibase-1.0-SNAPSHOT.jar | xargs -L 1 curl
+ */
+public class App {
+	public final static String URL_START;
+	public final static String KEY_NAME;
+	public final static String KEY_TEL;
+	static {
+		URL_START = System.getProperty("URL_START");
+		KEY_NAME = System.getProperty("KEY_NAME");
+		KEY_TEL = System.getProperty("KEY_TEL");
+	}
+	public static void main(String[] args) throws Exception {
+		vcard2clibase(System.in, System.out);
+	}
+
+	public static void vcard2clibase(InputStream in, PrintStream out) throws Exception {
+		try (VCardReader vcardReader = new VCardReader(in)) {
+			for (VCard vcard = vcardReader.readNext(); vcard != null; vcard = vcardReader.readNext()) {
+				String fullName;
+				try {
+					fullName = vcard.getFormattedName().getValue();
+				} catch (Exception e) {
+					fullName = null;
+				}
+				try {
+					List<Telephone> telephoneNumbers = vcard.getTelephoneNumbers();
+					for (Telephone telephoneNumber : telephoneNumbers) {
+						System.err.println("URL pour " + telephoneNumber.getText() + " (" + (fullName == null ? "sans nom" : fullName) + ")");
+						String getRequestStr = URL_START + (fullName == null ? "" : "&" + KEY_NAME + "=" + URLEncoder.encode(fullName, "UTF-8")) + "&" + KEY_TEL + "=" + URLEncoder.encode(
+							telephoneNumber
+							.getText()
+							.replaceAll("\\s*", "")
+							.replaceAll("\\(.*\\)", "")
+							.replaceAll("\\+", "00")
+						, "UTF-8");
+						out.println(getRequestStr);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					continue;
+				}
+			}
+		}
+	}
+}
